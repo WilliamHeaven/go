fec354f8829768d87b079f61709b5d9a2235c7d4
AllenX2018 <leloucharcher@163.com>
2020-02-18 15:28:37 +0800

    add testcase for SetIndent

--- api_tests/encoder_test.go
+++ api_tests/encoder_test.go
@@ -18,3 +18,15 @@ func TestEncoderHasTrailingNewline(t *testing.T) {
 	stdenc.Encode(1)
 	should.Equal(stdbuf.Bytes(), buf.Bytes())
 }
+
+func Test_set_indent(t *testing.T) {
+    should := require.New(t)
+    var buf1, buf2 bytes.Buffer
+    enc1 := jsoniter.NewEncoder(&buf1)
+    enc1.SetIndent("", " ")
+    enc1.Encode([]int{1, 2})
+    enc2 := json.NewEncoder(&buf2)
+    enc2.SetIndent("", " ")
+    enc2.Encode([]int{1, 2})
+    should.Equal(buf1.String(), buf2.String())
+}
